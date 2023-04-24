3fd8967ec804aea3472805dc38bbd2ef6b49484c
lolo859 <80057375+lolo859@users.noreply.github.com>
2023-04-24 15:52:59 +0200

    Delete module directory

--- module/browser.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-from termcolor import colored
-def execute(displaysplit,rep):
-    browser=rep[8::]
-    if type(browser)==str:
-        webbrowser.open(browser)
-    else:
-        print(colored("L'URl n'est pas bonne","red",))
